403aa1ccfd22263c2841c2e1cd1ebbdad2892826
N. Knietsch <69752231+knietsch@users.noreply.github.com>
2021-02-25 22:48:20 +0100

    Update speedchangeKeys.js
    addon has changed buttons id's

--- speedchangeKeys.js
+++ speedchangeKeys.js
@@ -7,8 +7,10 @@
 */
 
 document.addEventListener ("keypress", function (event) {
- event.key=="+"? document.querySelector('[data-name="speedup"]').click():{};
- event.key=="-"? document.querySelector('[data-name="slowdown"]').click():{};
+ event.key=="+"? document.getElementById('efyt-speed-plus').click():{};
+ event.key=="-"? document.getElementById('efyt-speed-minus').click():{};
+// event.key=="+"? document.querySelector('[data-name="speedup"]').click():{};
+// event.key=="-"? document.querySelector('[data-name="slowdown"]').click():{};
 });
-document.querySelector('[data-name="speedup"]').title="+";
-document.querySelector('[data-name="slowdown"]').title="-"
+//document.querySelector('[data-name="speedup"]').title="+";
+//document.querySelector('[data-name="slowdown"]').title="-"
