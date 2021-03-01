ede9ea90e0739973aaa5b5bdebb37cd647ee70dd
N. Knietsch <69752231+knietsch@users.noreply.github.com>
2021-03-01 17:22:23 +0100

    Update speedchangeKeys.js

--- speedchangeKeys.js
+++ speedchangeKeys.js
@@ -6,11 +6,17 @@
 
 */
 
-document.addEventListener ("keypress", function (event) {
- event.key=="+"? document.getElementById('efyt-speed-plus').click():{};
- event.key=="-"? document.getElementById('efyt-speed-minus').click():{};
-// event.key=="+"? document.querySelector('[data-name="speedup"]').click():{};
-// event.key=="-"? document.querySelector('[data-name="slowdown"]').click():{};
-});
-//document.querySelector('[data-name="speedup"]').title="+";
-//document.querySelector('[data-name="slowdown"]').title="-"
+if (document.getElementById('efyt-speed-plus')){
+  document.addEventListener ("keypress", function (event) {
+   event.key=="+"? document.getElementById('efyt-speed-plus').click():{};
+   event.key=="-"? document.getElementById('efyt-speed-minus').click():{};
+  });
+}
+else{
+  document.addEventListener ("keypress", function (event) {
+   event.key=="+"? document.querySelector('[data-name="speedup"]').click():{};
+   event.key=="-"? document.querySelector('[data-name="slowdown"]').click():{};
+  });
+  document.querySelector('[data-name="speedup"]').title="+";
+  document.querySelector('[data-name="slowdown"]').title="-"
+}
